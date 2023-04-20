923e62849ef40c33af306a8fc30ad38f05bc85a0
R-K-96 <124651427+R-K-96@users.noreply.github.com>
2023-04-20 11:33:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ class Animal {
 
 class Dog extends Animal {
 	bark() {
-		console.log("The " + this.species + " barks.");
+		console.log("woof");
 	}
 }
 
 class Cat extends Animal {
 
 	purr() {
-		console.log("The " + this.species + " purrs.");
+		console.log("purr");
 	}
 }
 
